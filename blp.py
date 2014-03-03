d6b8d3d65f828d8179e3a30c0e047fcb5af8b41c
Zubin Jelveh <zjelveh@gmail.com>
2014-03-02 22:18:03 -0500

    cleaned optimization code

--- blp.py
+++ blp.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import scipy.io as io
 import pandas as pd
-from scipy import optimize
+from scipy.optimize import minimize
 
 class BLP():
     def __init__(self):
@@ -72,6 +72,8 @@ class BLP():
         self.gmmvalold = 0
         self.gmmdiff = 1
 
+        self.iter = 0
+
     def init_theta(self):
         ## starting values for theta2. zero elements in the following matrix 
         ## correspond to coeff that will not be max over,i.e are fixed at zero.
@@ -95,27 +97,30 @@ class BLP():
 ##                               columns = ['F','int.income','int.income_sq','int.age',
 ##                                          'int.child'])
 
-    def gmmobj(self,theta2,theti,thetj):
+    def gmmobj(self, theta2, theti, thetj):
         # compute GMM objective function
         delta = self.meanval(theta2,theti,thetj)
         
-        ##% the following deals with cases were the min algorithm drifts into region where the objective is not defined
+        ##% the following deals with cases where the min algorithm drifts into region where the objective is not defined
         if max(np.isnan(delta)) == 1:
             f = 1e+10
         else:
-            temp1 = self.x1.T*self.IV
-            temp2 = delta.T*self.IV
-            self.theta1 = np.linalg.inv(temp1*self.invA*temp1.T)*temp1*self.invA*temp2.T
-            self.gmmresid = delta - self.x1*self.theta1
-            temp1 = self.gmmresid.T*self.IV
-            f = temp1*self.invA*temp1.T
-        print 'gmm objective:',f[0,0]
+            temp1 = self.x1.T * self.IV
+            temp2 = delta.T * self.IV
+            self.theta1 = np.linalg.inv(temp1 * self.invA * temp1.T) * temp1 * self.invA * temp2.T
+            self.gmmresid = delta - self.x1 * self.theta1
+            temp1 = self.gmmresid.T * self.IV
+            f = temp1 * self.invA * temp1.T
+
+        self.iter += 1
+        if self.iter % 10 == 0:
+            print 'gmm objective:', f[0,0]
         self.gmmvalnew = f[0,0]
         self.gmmdiff = np.abs(self.gmmvalold - self.gmmvalnew)
         self.gmmvalold = self.gmmvalnew
         return(f[0,0])
 
-    def meanval(self,theta2,theti,thetj):
+    def meanval(self, theta2, theti, thetj):
         if self.gmmdiff < 1e-6:
             etol = self.etol = 1e-13
         elif self.gmmdiff < 1e-3:
@@ -127,21 +132,20 @@ class BLP():
         i = 0
         theta2w = np.zeros((max(theti)+1,max(thetj)+1))
         for ind in range(len(theti)):
-                theta2w[theti[ind],thetj[ind]]=theta2[ind]
+                theta2w[theti[ind], thetj[ind]] = theta2[ind]
         self.expmu = np.exp(self.mufunc(theta2w))
         while norm > etol:
             pred_s_jt = self.mktsh()
-    ##        d_new = d_old + np.log(s_jt) - np.log(pred_s_jt)
-            self.d_new = np.multiply(self.d_old,self.s_jt)/pred_s_jt
+            self.d_new = np.multiply(self.d_old,self.s_jt) / pred_s_jt
             t = np.abs(self.d_new - self.d_old)
             norm = np.max(t)
             avgnorm = np.mean(t)
             self.d_old = self.d_new
-            i+=1
-        print '# of iterations for delta convergence:',i
+            i += 1
+##        print '# of iterations for delta convergence:', i
         return np.log(self.d_new)
 
-    def mufunc(self,theta2w):
+    def mufunc(self, theta2w):
         n,k = self.x2.shape
         j = theta2w.shape[1]-1
         mu = np.zeros((n,self.ns))
@@ -155,72 +159,88 @@ class BLP():
     def mktsh(self):
         # compute the market share for each product
         temp = self.ind_sh().T
-        f = sum(temp)/float(self.ns)
+        f = sum(temp) / float(self.ns)
         return f.T
     
     def ind_sh(self):
-        eg = np.multiply(self.expmu,np.kron(np.ones((1,self.ns)),self.d_old))
-        temp = np.cumsum(eg,0)
-        sum1 = temp[self.cdindex,:]
-        sum2=sum1
-        sum2[1:sum2.shape[0],:] = np.diff(sum1.T).T
-        denom1 = 1. / (1.+sum2)
-        denom = denom1[self.cdid,:]
-        return np.multiply(eg,denom)
+        eg = np.multiply(self.expmu, np.kron(np.ones((1, self.ns)), self.d_old))
+        temp = np.cumsum(eg, 0)
+        sum1 = temp[self.cdindex, :]
+        sum2 = sum1
+        sum2[1:sum2.shape[0], :] = np.diff(sum1.T).T
+        denom1 = 1. / (1. + sum2)
+        denom = denom1[self.cdid, :]
+        return np.multiply(eg, denom)
 
-    def jacob(self,mval,theta2w):
+    def jacob(self, *args):
+        print 'in jacob'
+        theta2w = np.zeros((max(self.theti) + 1, max(self.thetj) + 1))
+        for ind in range(len(self.theti)):
+            theta2w[self.theti[ind], self.thetj[ind]] = self.theta2[ind]
         self.expmu = np.exp(self.mufunc(theta2w))
         shares = self.ind_sh()
         n,K = self.x2.shape
-        J = theta2w.shape[1]-1
-        f1 = np.zeros((self.cdid.shape[0],K*(J+1)))
+        J = theta2w.shape[1] - 1
+        f1 = np.zeros((self.cdid.shape[0] , K * (J + 1)))
 
         for i in range(K):
-            xv = np.multiply(self.x2[:,i].reshape(self.nbrn*self.nmkt,1)*np.ones((1,self.ns)),self.v[self.cdid,self.ns*i:self.ns*(i+1)])
-            temp = np.cumsum(np.multiply(xv,shares),0)
-            sum1 = temp[self.cdindex,:]
-            sum1[1:sum1.shape[0],:] = np.diff(sum1.T).T
-            f1[:,i] = (np.mean((np.multiply(shares,xv-sum1[self.cdid,:])).T,0).T).flatten()
+            xv = np.multiply(self.x2[:, i].reshape(self.nbrn*self.nmkt, 1) * np.ones((1, self.ns)),
+                             self.v[self.cdid, self.ns*i:self.ns * (i + 1)])
+            temp = np.cumsum(np.multiply(xv, shares), 0)
+            sum1 = temp[self.cdindex, :]
+            sum1[1:sum1.shape[0], :] = np.diff(sum1.T).T
+            f1[:,i] = (np.mean((np.multiply(shares, xv - sum1[self.cdid,:])).T,0).T).flatten()
 
         for j in range(J):
-            d = self.demogr[self.cdid,self.ns*j:(self.ns*(j+1))]   
-            temp1 = np.zeros((self.cdid.shape[0],K))
+            d = self.demogr[self.cdid, self.ns * j:(self.ns * (j+1))]   
+            temp1 = np.zeros((self.cdid.shape[0], K))
             for i in range(K):
-                xd = np.multiply(self.x2[:,i].reshape(self.nbrn*self.nmkt,1)*np.ones((1,self.ns)),d)
-                temp = np.cumsum(np.multiply(xd,shares),0)
-                sum1 = temp[self.cdindex,:]
-                sum1 [1:sum1.shape[0],:] = np.diff(sum1.T).T
-                temp1[:,i] = (np.mean((np.multiply(shares,xd-sum1[self.cdid,:])).T,0).T).flatten()
-            f1[:,(K*(j+1)):(K*(j+2))] = temp1
+                xd = np.multiply(self.x2[:, i].reshape(self.nbrn * self.nmkt, 1) * np.ones((1, self.ns)), d)
+                temp = np.cumsum(np.multiply(xd, shares), 0)
+                sum1 = temp[self.cdindex, :]
+                sum1[1:sum1.shape[0], :] = np.diff(sum1.T).T
+                temp1[:, i] = (np.mean((np.multiply(shares, xd-sum1[self.cdid, :])).T, 0).T).flatten()
+            f1[:, (K * (j + 1)):(K * (j + 2))] = temp1
 
         rel = self.theti + (self.thetj - 1) * max(self.theti)
-        rel = np.array([0,1,2,3,4,5,6,7,9,12,14,15,17]).flatten()
+        rel = np.array([0, 1 , 2 , 3 , 4 , 5 , 6 , 7 , 9 , 12, 14, 15, 17]).flatten()
         f = np.zeros((self.cdid.shape[0],rel.shape[0]))
         n = 0
 
         for i in range(self.cdindex.shape[0]):
-            temp = shares[n:(self.cdindex[i]+1),:]
-            H1 = temp*temp.T
-            H = (np.diag(np.array(sum(temp.T)).flatten()) - H1)/self.ns
-            f[n:(self.cdindex[i]+1),:] = - np.linalg.inv(H)*f1[n:(self.cdindex[i]+1),rel]
+            temp = shares[n:(self.cdindex[i] + 1), :]
+            H1 = temp * temp.T
+            H = (np.diag(np.array(sum(temp.T)).flatten()) - H1) / self.ns
+            f[n:(self.cdindex[i]+1), :] = -np.linalg.inv(H)*f1[n:(self.cdindex[i] + 1), rel]
             n = self.cdindex[i] + 1
-            
         return f
 
     def varcov(self):
         N = self.x1.shape[0]
         Z = self.IV.shape[1]
-        theta2w = np.zeros((max(self.theti)+1,max(self.thetj)+1))
+        theta2w = np.zeros((max(self.theti) + 1, max(self.thetj) + 1))
         for ind in range(len(self.theti)):
-                theta2w[self.theti[ind],self.thetj[ind]]=self.theta2[ind]
-        temp = self.jacob(self.d_old,theta2w)
-        a = np.concatenate((self.x1.todense(),temp),1).T*self.IV
-        IVres = np.multiply(self.IV,self.gmmresid*np.ones((1,Z)))
+            theta2w[self.theti[ind], self.thetj[ind]] = self.theta2[ind]
+        temp = self.jacob(self.d_old, theta2w)
+        a = np.concatenate((self.x1.todense(), temp), 1).T * self.IV
+        IVres = np.multiply(self.IV, self.gmmresid * np.ones((1, Z)))
         b = IVres.T * IVres
-        f = np.linalg.inv(a*self.invA*a.T)*a*self.invA*b*self.invA*a.T*np.linalg.inv(a*self.invA*a.T)
+        inv_aAa = np.linalg.inv(a * self.invA * a.T)
+        f = inv_aAa * a * self.invA * b * self.invA * a.T * inv_aAa
         return f
-        
+
+    def iterate_optimization(self, opt_func, param_vec, args=(), method='Nelder-Mead'):
+        success = False
+        while not success:
+            blp.theta2 = minimize(opt_func, param_vec, args=args, method=method)
+            param_vec = blp.theta2
+            if res.success:
+                return res
+            
 if __name__ == '__main__':
     blp = BLP()
     init_theta = blp.init_theta()
-    blp.theta2 = optimize.fmin(blp.gmmobj,init_theta[0],(init_theta[1],init_theta[2]),maxiter=1e7)
+    res = blp.iterate_optimization(opt_func=blp.gmmobj,
+                                   param_vec=init_theta[0],
+                                   args=(init_theta[1], init_theta[2]))
+    
